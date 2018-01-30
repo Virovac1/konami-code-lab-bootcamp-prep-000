73a6759ee3836df3bd069a4f7cf7207decf03080
William Spangler <bspangs@hotmail.com>
2018-01-30 17:29:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  // keep track of index for key inputs outside of event handler
+let index=0
+console.log("Hello")
   // Write your JavaScript code inside the init() function
-
-}
+    const key = parseInt(e.detail || e.which)
+  }
